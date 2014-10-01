9b27f0f22317cbf916fd8b7e7520001abd959df5
saeed68gm <saeed68gm@gmail.com>
2014-10-01 15:44:28 -0400

    fixed the indexing issue, now it will return the index in the original array not the sorted array

--- binary_search_main.cpp
+++ binary_search_main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 
 //the merge function for merge sort
-void merge(float* input_array,int begin, int mid, int end,int* indices)
+void merge(float* input_array,int begin, int mid, int end,int* indices,int size)
 {
 	int n_1=mid-begin+1;
 	int n_2=end-mid;
 	float* Left=new float[n_1];
 	float* Right=new float[n_2];
+	int* temp_ind=new int[size];
+	memcpy(temp_ind,indices,sizeof(int)*(size));
 	for (int i=0;i<n_1;i++)
 	{
 		Left[i]=input_array[begin+i];
@@ -23,20 +25,20 @@ void merge(float* input_array,int begin, int mid, int end,int* indices)
 	//set the last element to infinity 2^32
 	Left[n_1]=4294967296;
 	Right[n_2]=4294967296;
-	int i=0,j=0;
+	int m=0,n=0;
 	for (int k=begin;k<=end;k++)
 	{
-		if (Left[i]<=Right[j])
+		if (Left[m]<=Right[n])
 		{
-			input_array[k]=Left[i];
-			indices[k]=begin+i;
-			i++;
+			input_array[k]=Left[m];
+			indices[k]=temp_ind[begin+m];
+			m++;
 		}
 		else
 		{
-			input_array[k]=Right[j];
-			indices[k]=mid+j+1;
-			j++;
+			input_array[k]=Right[n];
+			indices[k]=temp_ind[mid+n+1];
+			n++;
 		}
 	}
 	return;
@@ -45,7 +47,7 @@ void merge(float* input_array,int begin, int mid, int end,int* indices)
 //helper function to sort the array
 //takes a pointer to an array of floats and 
 //returns a pointer to the sorted array;
-void merge_sort(float* input_array,int begin, int end, int* indices)
+void merge_sort(float* input_array,int begin, int end, int* indices, int size)
 {
 	int length=end-begin;
 	if (input_array==NULL)
@@ -58,9 +60,9 @@ void merge_sort(float* input_array,int begin, int end, int* indices)
 	}
 	int mid=(begin+end)/2;
 	float* output=new float[length];
-	merge_sort(input_array,begin,mid,indices);
-	merge_sort(input_array,mid+1,end,indices);
-	merge(input_array,begin,mid,end,indices);
+	merge_sort(input_array,begin,mid,indices,size);
+	merge_sort(input_array,mid+1,end,indices,size);
+	merge(input_array,begin,mid,end,indices,size);
 	
 	return;
 }
@@ -86,7 +88,7 @@ void print_array(float* array, int length)
 //length: length of the input array
 //query: the query floating point value that we are searching for
 //Return value: the index of the value in the input array, -1 if item is not found
-int binary_search(float* input_array,int length, float query)
+int binary_search(float* input_array,int* indices,int length, float query)
 {
 	if (input_array==NULL)
 	{
@@ -103,7 +105,7 @@ int binary_search(float* input_array,int length, float query)
 		mid=low+(high-low)/2;
 		if (query==input_array[mid])
 		{
-			return mid;
+			return indices[mid];
 		}
 		else if (query<input_array[mid])
 		{
@@ -114,11 +116,11 @@ int binary_search(float* input_array,int length, float query)
 	}
 	if (query==input_array[high])
 	{
-		return high;
+		return indices[high];
 	}
 	else if (query==input_array[low])
 	{
-		return low;
+		return indices[low];
 	}
 	return -1;
 	
@@ -132,10 +134,14 @@ int main()
 	//float test_case[]={1.2548,2.4588,8.1,1.2547,0.58795,12,-1.2548,-1.2549,1,-1,2};
 	int length=sizeof(test_case)/sizeof(float);
 	int* indices=new int[length];
+	for (int i=0;i<length;i++)
+	{
+		indices[i]=i;
+	}
 	print_array(test_case,length);
-	merge_sort(test_case,0,length-1,indices);
+	merge_sort(test_case,0,length-1,indices,length);
 	print_array(test_case,length);
-	cout<<binary_search(test_case,length,-1.2549)<<endl;
+	cout<<binary_search(test_case,indices,length,88)<<endl;
 	getchar();
 	return 0;
 }
